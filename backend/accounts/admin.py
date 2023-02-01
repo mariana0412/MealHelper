8787178e5c5e59042b1d88b87f4bf6cc6d706fab
Mariana Piz <marianapiz0412@gmail.com>
2023-02-01 17:20:55 +0200

    Register models

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import MealsHelperUser, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'photo')
+
+
+class MealsHelperUserAdmin(UserAdmin):
+    model = MealsHelperUser
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'Other Personal info',
+            {
+                'fields': (
+                    'phone_number', 'avatar', 'products'
+                )
+            }
+        )
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(MealsHelperUser, MealsHelperUserAdmin)
