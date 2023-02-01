669a930f4a1c605ab9dcbb56ab81514b1f2658b6
Mariana Piz <marianapiz0412@gmail.com>
2023-02-01 21:59:26 +0200

    Return photo attribute and add description to Product

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 class Product(models.Model):
     name = models.CharField(max_length=40)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+    photo = models.ImageField(upload_to="productsPhotos", null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
