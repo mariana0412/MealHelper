bdb4242c7ee564c1675190a64d87b5e6b1072a84
Mariana Piz <marianapiz0412@gmail.com>
2023-02-01 14:51:48 +0200

    Rename path from token to login

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -3,7 +3,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from auth.views import MyTokenObtainPairView
 
 urlpatterns = [
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
