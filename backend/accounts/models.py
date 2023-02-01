c1884e34cffb531ea72b332bfa2edc18e9c73076
Mariana Piz <marianapiz0412@gmail.com>
2023-02-01 17:19:56 +0200

    Fix of typing mistake

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Product(models.Model):
     name = models.CharField(max_length=40)
-    photo = avatar = models.ImageField(upload_to="productsPhotos", null=True, blank=True)
+    photo = models.ImageField(upload_to="productsPhotos", null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
