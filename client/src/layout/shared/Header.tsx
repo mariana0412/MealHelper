26d388bd69adb02dfa7b704afc5250ebccf78d1d
Oleksandr Igumnov <igumssukr@gmail.com>
2023-02-02 14:33:19 +0200

    Header design was improved

--- client/src/layout/shared/Header.tsx
+++ client/src/layout/shared/Header.tsx
@@ -1,5 +1,5 @@
 import {FC} from "react";
-import {Container, Nav, Navbar, Offcanvas, Button, Image} from "react-bootstrap";
+import {Container, Nav, Navbar, Offcanvas, Button, Image, Row, Col} from "react-bootstrap";
 
 export const Header: FC = () => {
   return (
@@ -16,8 +16,9 @@ export const Header: FC = () => {
           <Navbar.Offcanvas id={`offcanvasNavbar-expand-lg`}
                             aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
                             placement={"start"}
+                            className={"bg-dark text-white"}
           >
-            <Offcanvas.Header closeButton>
+            <Offcanvas.Header closeButton closeVariant={"white"}>
               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
                 Menu
               </Offcanvas.Title>
@@ -28,7 +29,7 @@ export const Header: FC = () => {
                 <Nav.Link href={"#link"}>Products</Nav.Link>
                 <Nav.Link href={"#link"}>About</Nav.Link>
               </Nav>
-              <div>
+              <div className={"mt-4 mt-lg-0"}>
                 <Button variant={"primary"} className={"me-2"}>Sign In</Button>
                 <Button variant={"outline-light"}>Sign Up</Button>
               </div>
@@ -39,10 +40,32 @@ export const Header: FC = () => {
       <Container fluid className={"header-image-container p-0"}>
         <Image src={`${process.env.PUBLIC_URL}/header-image.jpg`} alt={"Food"}
                className={"header-image img-fluid vh-100 vw-100"} />
-        <div className={"header-text"}>
-          <h1>Meal Helper</h1>
-          <h2 className={"mt-3"}>Delicious recipes. Daily updated</h2>
-          <Button variant={"danger"} size={"lg"} className={"mt-5"}>Look for a recipe</Button>
+        <div className={"header-content p-3 p-md-5 flex-wrap flex-xl-nowrap justify-content-center justify-content-xl-start"}>
+          <div className={"header-text align-items-center align-items-xl-start mt-5"}>
+            <h1>Meal Helper</h1>
+            <h2 className={"mt-3"}>Delicious recipes. Daily updated</h2>
+            <Button variant={"danger"} size={"lg"} className={"mt-5"}>Look for a recipe</Button>
+          </div>
+          <div className={"user-instructions mt-5"}>
+            <div className={"user-instructions-list"}>
+              <div className={"user-instructions-item"}>
+                <Image src={`${process.env.PUBLIC_URL}/recipe-book.png`} alt={"Look for a recipe"} />
+                <p>Look for a recipe</p>
+              </div>
+              <div className={"user-instructions-item"}>
+                <Image src={`${process.env.PUBLIC_URL}/grocery.png`} alt={"Check ingredients"} />
+                <p>Check what ingredients are needed</p>
+              </div>
+              <div className={"user-instructions-item"}>
+                <Image src={`${process.env.PUBLIC_URL}/recipe.png`} alt={"Look how to cook"} />
+                <p>Read how to cook the selected dish</p>
+              </div>
+              <div className={"user-instructions-item"}>
+                <Image src={`${process.env.PUBLIC_URL}/meal.png`} alt={"Enjoy your meal"} />
+                <p>Enjoy your meal</p>
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </header>
